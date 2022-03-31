7e83e69641ee93310f0d006458fd6a92c98a6ad7
longks2312k <longks2312k@gmail.com>
2022-03-31 09:28:23 +0700

    picture p1

--- src/components/container/Picture/Picture.tsx
+++ src/components/container/Picture/Picture.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Col, Row } from 'antd';
 import './Picture.css'
 
 const Picture = () => {
     return (
         <div className='picture'>
-            
+            <div className="picture-Title">
+                <h2>Picture</h2>
+            </div>
+            <Row className='list-item'>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+            </Row>
+            {/* <Row className='list-item'>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+            </Row>
+            <Row className='list-item'>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+                <img className='picture-item' src='https://www.dungplus.com/wp-content/uploads/2019/12/girl-xinh-1-480x600.jpg'>
+                    
+                </img>
+            </Row> */}
         </div>
     );
 };
