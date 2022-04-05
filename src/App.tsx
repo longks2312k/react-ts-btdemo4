6b96032f767ce86492b4637450f2328d8814caf9
longks2312k <longks2312k@gmail.com>
2022-04-05 15:35:49 +0700

    setup Redux-Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.min.css';
 import Profile from './pages/Profile';
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <div>
       <Profile/>
+      {/* <Counter /> */}
     </div>
   );
 }
