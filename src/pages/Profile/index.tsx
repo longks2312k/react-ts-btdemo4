eb62d53690e9f77fa5032b27e0a1929c59ac6d61
longks2312k <longks2312k@gmail.com>
2022-04-12 17:14:22 +0700

    Go to Top

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,31 +1,74 @@
-import React, { useContext } from 'react';
-import About from '../../components/container/About';
-import Biography from '../../components/container/biography';
-import Contact from '../../components/container/contact';
-import Events from '../../components/container/events';
-import Friends from '../../components/container/friends';
-import Information from '../../components/container/information';
-import Picture from '../../components/container/Picture';
-import Footer from '../../components/footer';
-import Header from '../../components/header';
-import { ThemeContext } from '../../contexts/ThemeContext';
-import './style.css'
+import React, { useContext, useEffect, useState } from "react";
+import 'antd'
+import About from "../../components/container/About";
+import Biography from "../../components/container/biography";
+import Contact from "../../components/container/contact";
+import Events from "../../components/container/events";
+import Friends from "../../components/container/friends";
+import Information from "../../components/container/information";
+import Picture from "../../components/container/Picture";
+import Footer from "../../components/footer";
+import Header from "../../components/header";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import "./style.css";
+import { Button } from "antd";
+import { UpCircleFilled } from "@ant-design/icons";
 
 const Profile = () => {
-    const {bgrColor} = useContext(ThemeContext);
-    return (
-        <div className='bgr-color' style={{backgroundColor: `${bgrColor}`}}>
-            <Header/>
-            <Information/>
-            <Contact/>
-            <Friends/>
-            <Picture/>
-            <About/>
-            <Biography/>
-            <Events/>
-            <Footer/>
-        </div>
-    );
+  const { bgrColor, itemColor, textColor } = useContext(ThemeContext);
+  //Listen DOM Event
+  const [showGoToTop, setShowGoToTop] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    //Scroll
+    const handleScroll = () => {
+        setShowGoToTop(window.scrollY >= 200);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    //Resize
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    //cleanup fs
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div className="bgr-color" style={{ backgroundColor: `${bgrColor}` }}>
+      <Header />
+      <Information />
+      <Contact />
+      <Friends />
+      <Picture />
+      <About />
+      <Biography />
+      <Events />
+      <Footer />
+      {showGoToTop && (
+          <Button
+            style={{
+              position: 'fixed',
+              right: 20,
+              bottom: 20,
+              height: '50px',
+              width: '50px',
+              borderRadius: '25px',
+              backgroundColor:`${itemColor}`,
+              color:`${textColor}`
+            }}
+            icon={<UpCircleFilled style={{color:`${textColor}`,fontSize: 20}}/>}
+            onClick={() => window.scrollTo({
+              top: 0, 
+              behavior: 'smooth'
+            })}
+          />
+        )}
+    </div>
+  );
 };
 
 export default Profile;
