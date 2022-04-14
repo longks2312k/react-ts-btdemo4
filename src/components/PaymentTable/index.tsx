57337e93819a90c652eab8c7535587384ecdb4d1
longks2312k <longks2312k@gmail.com>
2022-04-14 17:21:54 +0700

    Using thunk get api

--- src/components/PaymentTable/index.tsx
+++ src/components/PaymentTable/index.tsx
@@ -1,76 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Table, Radio, Divider, Row, Col } from "antd";
+import React, { useContext, useEffect } from "react";
+import { Row, Col } from "antd";
 import "./style.css";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { getProducts } from "./thunk";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { listProductsSelector } from "./slice";
-
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    render: (text: string) => <a>{text}</a>,
-  },
-  {
-    title: "Image",
-    dataIndex: "Image",
-    render: (image: string) => <img src={`${image}`} />,
-  },
-  {
-    title: "Date",
-    dataIndex: "Date",
-    render: (date: number) => <a>{date}</a>,
-  },
-];
-
-interface DataType {
-  key: React.Key;
-  name: string;
-  date: number;
-  image: string;
-}
-
-const data: DataType[] = [
-  {
-    key: "1",
-    name: "John Brown",
-    date: 1649907330,
-    image:
-      "https://thuthuattienich.com/wp-content/uploads/2017/02/anh-dai-dien-facebook-doc-3.jpg",
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    date: 1649907330,
-    image:
-      "https://thuthuattienich.com/wp-content/uploads/2017/02/anh-dai-dien-facebook-doc-3.jpg",
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    date: 1649907330,
-    image:
-      "https://thuthuattienich.com/wp-content/uploads/2017/02/anh-dai-dien-facebook-doc-3.jpg",
-  },
-  {
-    key: "4",
-    name: "Disabled User",
-    date: 1649907330,
-    image:
-      "https://thuthuattienich.com/wp-content/uploads/2017/02/anh-dai-dien-facebook-doc-3.jpg",
-  },
-];
-
-const rowSelection = {
-  onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-    // console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-  },
-  getCheckboxProps: (record: DataType) => ({
-    // disabled: record.name === 'Disabled User', // Column configuration not to be checked
-    // name: record.name,
-  }),
-};
 
 const PaymentTable = () => {
   const { itemColor } = useContext(ThemeContext);
@@ -85,15 +19,6 @@ const PaymentTable = () => {
 
   return (
     <div className="table-payment" style={{ backgroundColor: `${itemColor}` }}>
-      {/* <Divider />
-      <Table
-        rowSelection={{
-          type: "checkbox",
-          ...rowSelection,
-        }}
-        columns={columns}
-        dataSource={data}
-      /> */}
       {loading && <div>loading...</div>}
       {product.map((item) =>
           <Row key={item.id} className="table-item" style={{backgroundColor: '#f4f4f7'}}>
